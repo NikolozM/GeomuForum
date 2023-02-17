43518076b544fb6331a389141358f9a7315b657c
Nikoloz Mazmishvili <MY_NAME@example.commazmishvilinika888@gmail.com>
2023-02-17 18:58:15 +0400

    ok

--- GeomuForum/src/App.jsx
+++ GeomuForum/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import Topbar from './Topbar';
 import Footer from './Footer';
 import GeneralInformation from './components/GeneralInformation';
-
+import ServerCurrencies from './components/ServerCurrencies'
 function App() {
   const [activeTab, setActiveTab] = useState('General Information');
 
@@ -13,23 +13,19 @@ function App() {
   };
 
   return (
-    <div>
-      <div className='my-component'>
-        <Topbar handleTabClick={handleTabClick} setActiveTab={setActiveTab} activeTab={activeTab} />
-        <div className='topbar-content'>
-          {/* Content for the active tab goes here */}
-          {activeTab === 'General Information' && (
-              <GeneralInformation />
-          )}
-          {activeTab === 'Server Currencies' && <div>Server Currencies content goes here</div>}
-          {activeTab === 'Event System' && <div>Event System content goes here</div>}
-          {activeTab === 'Box System' && <div>Box System content goes here</div>}
-          {activeTab === 'Reset Information' && <div>Reset Information content goes here</div>}
-          {activeTab === 'Game Commands' && <div>Game Commands content goes here</div>}
-          {activeTab === 'Vip System' && <div>Bosses content goes here</div>}
-        </div>
+    <div className='my-component'>
+      <Topbar handleTabClick={handleTabClick} setActiveTab={setActiveTab} activeTab={activeTab} />
+      <div className='main-content'>
+        {/* Content for the active tab goes here */}
+        {activeTab === 'General Information' && <GeneralInformation />}
+        {activeTab === 'Server Currencies' && <div><ServerCurrencies/></div>}
+        {activeTab === 'Event System' && <div>Event System content goes here</div>}
+        {activeTab === 'Box System' && <div>Box System content goes here</div>}
+        {activeTab === 'Reset Information' && <div>Reset Information content goes here</div>}
+        {activeTab === 'Game Commands' && <div>Game Commands content goes here</div>}
+        {activeTab === 'Vip System' && <div>Bosses content goes here</div>}
       </div>
-        <Footer />
+      <Footer />
     </div>
   );
 }
--- GeomuForum/src/Topbar.jsx
+++ GeomuForum/src/Topbar.jsx
@@ -5,48 +5,50 @@ import GameHistory from './components/GeneralInformation';
 function Topbar({ handleTabClick, setActiveTab, activeTab }) {
   return (
     <div className='topbar'>
-      <ul
-        className={`topbar-section ${activeTab === 'General Information' ? 'active' : ''}`}
-        onClick={() => handleTabClick('General Information')}
-      >
-        <h2>General Information</h2>
-      </ul>
-      <ul
-        className={`topbar-section ${activeTab === 'Server Currencies' ? 'active' : ''}`}
-        onClick={() => handleTabClick('Server Currencies')}
-      >
-        <h2>Server Currencies</h2>
-      </ul>
-      <ul
-        className={`topbar-section ${activeTab === 'Event System' ? 'active' : ''}`}
-        onClick={() => handleTabClick('Event System')}
-      >
-        <h2>Event System</h2>
-      </ul>
-      <ul
-        className={`topbar-section ${activeTab === 'Box System' ? 'active' : ''}`}
-        onClick={() => handleTabClick('Box System')}
-      >
-        <h2>Box System</h2>
-      </ul>
-      <ul
-        className={`topbar-section ${activeTab === 'Reset Information' ? 'active' : ''}`}
-        onClick={() => handleTabClick('Reset Information')}
-      >
-        <h2>Reset Information</h2>
-      </ul>
-      <ul
-        className={`topbar-section ${activeTab === 'Game Commands' ? 'active' : ''}`}
-        onClick={() => handleTabClick('Game Commands')}
-      >
-        <h2>Game Commands</h2>
-      </ul>
-      <ul
-        className={`topbar-section ${activeTab === 'Vip System' ? 'active' : ''}`}
-        onClick={() => handleTabClick('Vip System')}
-      >
-        <h2>Vip System</h2>
-      </ul>
+      <div className='topbar-categories'>
+        <ul
+          className={`topbar-section ${activeTab === 'General Information' ? 'active' : ''}`}
+          onClick={() => handleTabClick('General Information')}
+        >
+          <h2>General Information</h2>
+        </ul>
+        <ul
+          className={`topbar-section ${activeTab === 'Server Currencies' ? 'active' : ''}`}
+          onClick={() => handleTabClick('Server Currencies')}
+        >
+          <h2>Server Currencies</h2>
+        </ul>
+        <ul
+          className={`topbar-section ${activeTab === 'Event System' ? 'active' : ''}`}
+          onClick={() => handleTabClick('Event System')}
+        >
+          <h2>Event System</h2>
+        </ul>
+        <ul
+          className={`topbar-section ${activeTab === 'Box System' ? 'active' : ''}`}
+          onClick={() => handleTabClick('Box System')}
+        >
+          <h2>Box System</h2>
+        </ul>
+        <ul
+          className={`topbar-section ${activeTab === 'Reset Information' ? 'active' : ''}`}
+          onClick={() => handleTabClick('Reset Information')}
+        >
+          <h2>Reset Information</h2>
+        </ul>
+        <ul
+          className={`topbar-section ${activeTab === 'Game Commands' ? 'active' : ''}`}
+          onClick={() => handleTabClick('Game Commands')}
+        >
+          <h2>Game Commands</h2>
+        </ul>
+        <ul
+          className={`topbar-section ${activeTab === 'Vip System' ? 'active' : ''}`}
+          onClick={() => handleTabClick('Vip System')}
+        >
+          <h2>Vip System</h2>
+        </ul>
+      </div>
     </div>
   );
 }
